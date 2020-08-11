ea638ad773357ee6d2b37aed518a050eb381391c
Andrey <KonkeyDong37@gmail.com>
2020-08-11 19:04:39 +0500

    Add custom class and style for cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Андрей on 11.08.2020.
+//  Copyright © 2020 Андрей. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
