1c70692cf3733241cde99614c3d8108c65a80780
Andrey <KonkeyDong37@gmail.com>
2020-08-11 18:35:34 +0500

    Add styles for image, add launch screen and add app icons

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -34,8 +34,17 @@ class MainViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "preview_image")
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     /*
