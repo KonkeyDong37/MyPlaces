8cd619be9d76794ed37a90ae2c26d6c3ca8835e5
Andrey <KonkeyDong37@gmail.com>
2020-08-12 16:09:43 +0500

    Add styles for text fields and cells in New places view

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Андрей on 12.08.2020.
+//  Copyright © 2020 Андрей. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    // Скрываем клавиатуру по нажатию Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
